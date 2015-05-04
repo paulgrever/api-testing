a59de49b34f6d7c3da113c49c71773a90285556a
Paul Grever <paulgrever@gmail.com>
2015-05-04 09:36:09 -0600

    added a first test for minitest

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -4,7 +4,10 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
   test '#index' do 
     get :index, format: :json
     items = JSON.parse(response.body)
+    item = items.first
     assert_response :success
+    assert_equal 'Cow', item['name']
+    assert_equal 'I am a cow.', item['description']
   end
   # test "the truth" do
   #   assert true
