754772eee145cbd824f257393bf683e5aed99017
Paul Grever <paulgrever@gmail.com>
2015-05-04 09:48:26 -0600

    test for show controller added with symbolized name

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -9,6 +9,15 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
     assert_equal 'Cow', item['name']
     assert_equal 'I am a cow.', item['description']
   end
+
+  test '#show' do 
+    item = Item.create(name: "Dog", description: "This is a dog")
+    get :show, format: :json, id: item.id
+    first_item = JSON.parse(response.body, symbolize_names: true) 
+    assert_response :success
+    assert_equal "Dog", first_item[:name]
+    assert_equal "This is a dog", first_item[:description]
+  end
   # test "the truth" do
   #   assert true
   # end
